f60d2cf711820714b854f0d11f7d08b529463e5e
borgchrist1 <christian.borg@outlook.com>
2016-12-11 13:11:22 +0100

    database.php added

---
+++ Database.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *
+ */
+class Database
+{
+    private $host = "127.0.0.1";
+    private $dbName = "";
+    private $username = "root";
+    private $password = "root";
+    private $socetType = "mysql";
+
+    private $instance = NULL;
+
+    function __construct()
+    {
+        if ($this->instance == NULL) {
+            try {
+                $db = new PDO (''.$this->socetType .':host='. $this->host .';dbname='. $this->dbName .'', $this->username, $this->password );
+
+                $this->instance = $db;
+            } catch (Exception $e) {
+                die($e->getMessage());
+            }
+        }
+    }
+
+    public function query($sql)
+    {
+        $query = $this->instance->prepare($sql);
+        $query->exicute();
+
+        return $query;
+    }
+}

bd2af69b2d469a27316c858817ce5a89364aafd4
borgchrist1 <christian.borg@outlook.com>
2016-12-23 11:19:09 +0100

    added forms class

---
+++ classes/Forms.php
@@ -0,0 +1,46 @@
+<?php
+
+class Forms
+{
+
+    public function cleanData ($data)
+    {
+        return trim(stripcslashes($data));
+
+    }
+    
+    Public function checkEmail ($email)
+    {
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return false;
+        }
+    }
+
+    Public function checkPassword ($password, $rePassword) {
+        if ($password !== $rePassword){
+            return false;
+        }
+
+        return true;
+    }
+
+    public  function encryptPassword ($password)
+    {
+        return md5($password);
+    }
+
+    Public function uniqueEmail ($email){
+        $db = new Database();
+
+        $query = $db->query("SELECT * FROM users
+            WHERE email='".$email."'");
+
+        if (count($query) !== 0){
+            return false;
+        }
+        return true;
+    }
+
+
+}

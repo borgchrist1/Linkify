cbb67eef71024413bf38e346f33a947fa2b8ae08
borgchrist1 <christian.borg@outlook.com>
2017-01-01 23:45:36 +0100

    -

--- resources/lib/settings.php
+++ resources/lib/settings.php
@@ -3,18 +3,17 @@ session_start();
 require "../../classes/Forms.php";
 if ($_SERVER["REQUEST_METHOD"] === "POST"){
     $validateForm = new Forms();
+
     if (!isset($_POST["oldPassword"])){
-//        $name = $validateForm->cleanData($_POST["name"]);
-//        $username = $validateForm->cleanData($_POST["username"]);
-//        $email = $validateForm->checkEmail($_POST["email"]);
-//        $email = $validateForm->uniqueEmail($email);
-//        $password = $validateForm->encryptPassword($_POST["password"]);
-//        $user = new User();
-//        $user->insertChanges($_SESSION["id"], $name, $username, $email);
+        $name = $validateForm->cleanData($_POST["name"]);
+        $username = $validateForm->cleanData($_POST["username"]);
+        $email = $validateForm->checkEmail($_POST["email"]);
+        $email = $validateForm->uniqueEmail($email);
+        $password = $validateForm->encryptPassword($_POST["password"]);
+        $user = new User();
+        $user->insertChanges($_SESSION["id"], $name, $username, $email);
         $dir = "resources/img/users/";
         $path = scandir("../../{$dir}");
-
-
 
         foreach ($path as $folder){
             if (file_exists("../../{$dir}/{$_SESSION["id"]}")) {

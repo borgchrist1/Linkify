cc0627c8212b5ab1a49afb135e1b86e5839c037c
borgchrist1 <christian.borg@outlook.com>
2017-01-01 23:35:48 +0100

    added comments class

---
+++ classes/Comment.php
@@ -0,0 +1,75 @@
+<?php
+
+class Comment
+{
+    private $id;
+    private $comment;
+    private $userId;
+    private $postId;
+    private $vote;
+
+    public function __construct($id = null, $comment = null, $userId = null, $postId = null, $vote = null)
+    {
+        if (!$id = null) $this->id = $id;
+        if (!$comment = null) $this->comment = $comment;
+        if (!$userId = null) $this->userId = $userId;
+        if (!$postId = null) $this->postId = $postId;
+        if(!$vote = null) $this->vote = $vote;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    public function getPostId()
+    {
+        return $this->postId;
+    }
+
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    public function getVote()
+    {
+        return $this->vote;
+    }
+
+    public function createComment()
+    {
+        $db = new Database();
+        $query = $db->query("INSERT INTO comments (comment, userId, postId)
+            VALUES ('".$this->getComment()."','".$this->getUserId()."','".$this->getPostId()."')");
+        return $query;
+    }
+
+    public function addVote()
+    {
+        $db = new Database();
+        $query = $db->query("INSERT INTO comments (vote)
+            VALUES ('".$this->getVote()."')");
+        return $query;
+    }
+
+    public function getComments($postId)
+    {
+        $db = new Database();
+        $query = $db->getObjects("SELECT * FROM comment
+            WHERE user_id ='".$postId."'", "comments");
+
+       $result = usort($query, function($a, $b)
+        {
+            return strcmp($a->vote, $b->vote);
+        });
+
+       return $result;
+    }
+
+}

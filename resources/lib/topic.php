de409888a3cad8d361bde3c36d102bf9784263d4
borgchrist1 <christian.borg@outlook.com>
2017-01-02 13:18:29 +0100

    added post handling for comments

---
+++ resources/lib/topic.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+require "../../classes/Forms.php";
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+    $validateForm = new Forms();
+    $comment = $validateForm->cleanData($_POST["comment"]);
+    $newComment = new Comment($id = null, $comment, $_SESSION["id"], $_GET["id"]);
+    $newComment->createComment();
+    header("Location: ../../topic.php");
+}
